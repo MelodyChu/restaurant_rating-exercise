e87fce2e3cc6bcbfa7eb42d518cf9eda2b0248aa
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:52:55 -0800

    Added new features: - User input - Validation for user input ratings

--- ratings_withdict.py
+++ ratings_withdict.py
@@ -13,15 +13,41 @@ def input_restaurant(filename):
         restaurant, rating = restaurant_list
         restaurant_dict[restaurant] = rating
     # print restaurant_dict
+    print "1) See a sorted list of ratings \n2) Add new restaurant and rate it \nType 'q' to quit"
+    user_choice = raw_input("What would you like to do? ")
+
+    if user_choice == '1':
+        for restaurant, ratings in sorted(restaurant_dict.items()):
+            print '{} has a rating of {}'.format(restaurant, ratings)
+
+    elif user_choice == '2':
+        new_restaurant = raw_input("Enter name of restaurant: ")
+   
+        while True:
+            try:
+                new_rating = raw_input("\n How would you rate this restaurant from 1-5? ")
+                if int(new_rating) in range(1, 6):
+                    break
+                print "Please provide a value between 1 and 5!"
+            except ValueError:
+                print "Invalid input. Please provide a value between 1 and 5!"
+        restaurant_dict[new_restaurant] = new_rating
+
+        for restaurant, ratings in sorted(restaurant_dict.items()):
+            print '{} has a rating of {}'.format(restaurant, ratings)
+
+    elif user_choice in 'Qq':
+        print "Session terminated"
+        return
+
+    # else:
+
+
 
     # getting user input
-    new_restaurant = raw_input("Enter name of restaurant: ")
-    new_rating = raw_input("How would you rate this restaurant from 1-5? ")
+    
 
-    restaurant_dict[new_restaurant] = new_rating
     # sorting by the items
     # turns into tuples
-    for restaurant, ratings in sorted(restaurant_dict.items()):
-        print '{} has a rating of {}'.format(restaurant, ratings)
 
 input_restaurant("scores.txt")
